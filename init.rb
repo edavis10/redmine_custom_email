87845d03fcf761537613f839f6cf6d78d8c0bdf9
Eric Davis <edavis@littlestreamsoftware.com>
2009-02-13 14:38:38 -0800

    Generated new plugin

---
+++ init.rb
@@ -0,0 +1,12 @@
+require 'redmine'
+
+Redmine::Plugin.register :redmine_custom_email do
+  name 'Custom Email plugin'
+  author 'Eric Davis'
+  author_url 'http://www.littlestreamsoftware.com'
+  description "Plugin to customize the way email is delivered"
+  version '0.1.0'
+
+  requires_redmine :version_or_higher => '0.8.0'
+
+end
