79b2885ddb4a296d4d98ad8ea2b3eeae006d63b0
Eric Davis <edavis@littlestreamsoftware.com>
2009-02-16 12:13:45 -0800

    Added a helper to render the Deliverable subject. #2136

--- app/helpers/custom_email_helper.rb
+++ app/helpers/custom_email_helper.rb
@@ -1,2 +1,14 @@
 module CustomEmailHelper
+
+  def deliverable_subject(deliverable, html=true)
+    return '' unless Object.const_defined?("Deliverable") && @issue.deliverable
+      
+    if html
+      return "<li>#{ l(:field_deliverable_subject) }: #{ @issue.deliverable.subject }</li>"
+    else
+      return "#{ l(:field_deliverable_subject) }: #{ @issue.deliverable.subject }"
+    end
+  end
+
+
 end
