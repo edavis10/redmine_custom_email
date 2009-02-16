a45be82adc835a052f5533dade292155b1300624
Eric Davis <edavis@littlestreamsoftware.com>
2009-02-16 12:35:59 -0800

    Moved more specs to the shared behavior. #2136

--- spec/lib/custom_email_mailer_patch_spec.rb
+++ spec/lib/custom_email_mailer_patch_spec.rb
@@ -1,35 +1,68 @@
 require File.dirname(__FILE__) + '/../spec_helper'
 
-describe 'with a Deliverable', :shared => true do
-  before(:each) do
-    @issue.deliverable = mock_model(Deliverable, :subject => "Take over the world", :labor_budget => 200.0)
+describe 'a custom email', :shared => true do
+  it 'should use the Redmine method if no recipient is passed'
+  
+  it 'should set the recipient to only one user' do
+    mail = Mailer.create_issue_add(@issue, @author.mail)
+    mail.bcc.size.should eql(1)
+  end
+
+  it 'should display the Deliverable subject if the Budget plugin is present' do
+    @issue.deliverable = mock_model(Deliverable, :subject => "Take over the world")
+    mail = create_mail
+    mail.body.should match(/take over the world/i)
+  end
+
+  it 'should show the total time spent on the issue if the user has the :view_time_entries permission' do
+    @issue.estimated_hours = 20
+    @issue.should_receive(:spent_hours).at_least(:twice).and_return(10)
+    @author.should_receive(:allowed_to?).at_least(:twice).with(:view_time_entries, @project).and_return(true)
+    User.should_receive(:find_by_mail).with(@author.mail).and_return(@author)
+
+    mail = create_mail
+    mail.body.should match(/10 of 20.0 hours/i)
+  end
+
+  it 'should not show the total time spent on the issue if the user doesnt have the :view_time_entries permission' do
+    @issue.estimated_hours = 20
     @author.stub!(:allowed_to?).with(:view_time_entries, @project).and_return(false)
-    User.stub!(:find_by_mail).with(@author.mail).and_return(@author)
+    User.should_receive(:find_by_mail).with(@author.mail).and_return(@author)
+
+    mail = create_mail
+    mail.body.should_not match(/hours/i)
   end
 
-  describe 'with the :manage_budget permission' do
+  describe 'with a Deliverable' do
     before(:each) do
-      @author.stub!(:allowed_to?).with(:manage_budget, @project).and_return(true)
+      @issue.deliverable = mock_model(Deliverable, :subject => "Take over the world", :labor_budget => 200.0)
+      @author.stub!(:allowed_to?).with(:view_time_entries, @project).and_return(false)
+      User.stub!(:find_by_mail).with(@author.mail).and_return(@author)
     end
 
-    it 'should show the labor budget' do
-      mail = create_mail
-      mail.body.should match(/\$200.0/i)
-    end
-  end
+    describe 'with the :manage_budget permission' do
+      before(:each) do
+        @author.stub!(:allowed_to?).with(:manage_budget, @project).and_return(true)
+      end
 
-  describe 'without the :manage_budget permission' do
-    before(:each) do
-      @author.stub!(:allowed_to?).with(:manage_budget, @project).and_return(false)
+      it 'should show the labor budget' do
+        mail = create_mail
+        mail.body.should match(/\$200.0/i)
+      end
     end
 
-    it 'should not show the labor budget' do
-      mail = create_mail
-      mail.body.should_not match(/200.0/i)
+    describe 'without the :manage_budget permission' do
+      before(:each) do
+        @author.stub!(:allowed_to?).with(:manage_budget, @project).and_return(false)
+      end
+
+      it 'should not show the labor budget' do
+        mail = create_mail
+        mail.body.should_not match(/200.0/i)
+      end
     end
   end
 end
-
 
 describe Mailer, "#issue_add" do
   def create_mail
@@ -44,39 +77,7 @@ describe Mailer, "#issue_add" do
     @author.stub!(:allowed_to?).with(:manage_budget, @project).and_return(false)
   end
 
-  it 'should use the Redmine method if no recipient is passed'
-  
-  it 'should set the recipient to only one user' do
-    mail = Mailer.create_issue_add(@issue, @author.mail)
-    mail.bcc.size.should eql(1)
-  end
-
-  it 'should display the Deliverable subject if the Budget plugin is present' do
-    @issue.deliverable = mock_model(Deliverable, :subject => "Take over the world")
-    mail = Mailer.create_issue_add(@issue, @author.mail)
-    mail.body.should match(/take over the world/i)
-  end
-
-  it 'should show the total time spent on the issue if the user has the :view_time_entries permission' do
-    @issue.estimated_hours = 20
-    @issue.should_receive(:spent_hours).at_least(:twice).and_return(10)
-    @author.should_receive(:allowed_to?).at_least(:twice).with(:view_time_entries, @project).and_return(true)
-    User.should_receive(:find_by_mail).with(@author.mail).and_return(@author)
-
-    mail = Mailer.create_issue_add(@issue, @author.mail)
-    mail.body.should match(/10 of 20.0 hours/i)
-  end
-
-  it 'should not show the total time spent on the issue if the user doesnt have the :view_time_entries permission' do
-    @issue.estimated_hours = 20
-    @author.stub!(:allowed_to?).with(:view_time_entries, @project).and_return(false)
-    User.should_receive(:find_by_mail).with(@author.mail).and_return(@author)
-
-    mail = Mailer.create_issue_add(@issue, @author.mail)
-    mail.body.should_not match(/hours/i)
-  end
-
-  it_should_behave_like 'with a Deliverable'
+  it_should_behave_like 'a custom email'
 end
 
 describe Mailer, "#issue_edit" do
@@ -101,37 +102,5 @@ describe Mailer, "#issue_edit" do
                           :details => @journal_details)
   end
 
-  it 'should use the Redmine method if no recipient is passed'
-  
-  it 'should set the recipient to only one user' do
-    mail = Mailer.create_issue_edit(@journal, @author.mail)
-    mail.bcc.size.should eql(1)
-  end
-
-  it 'should display the Deliverable subject if the Budget plugin is present' do
-    @issue.deliverable = mock_model(Deliverable, :subject => "Take over the world")
-    mail = Mailer.create_issue_edit(@journal, @author.mail)
-    mail.body.should match(/take over the world/i)
-  end
-
-  it 'should show the total time spent on the issue if the user has the :view_time_entries permission' do
-    @issue.estimated_hours = 20
-    @issue.should_receive(:spent_hours).at_least(:twice).and_return(10)
-    @author.should_receive(:allowed_to?).at_least(:twice).with(:view_time_entries, @project).and_return(true)
-    User.should_receive(:find_by_mail).with(@author.mail).and_return(@author)
-
-    mail = Mailer.create_issue_edit(@journal, @author.mail)
-    mail.body.should match(/10 of 20.0 hours/i)
-  end
-
-  it 'should not show the total time spent on the issue if the user doesnt have the :view_time_entries permission' do
-    @issue.estimated_hours = 20
-    @author.stub!(:allowed_to?).with(:view_time_entries, @project).and_return(false)
-    User.should_receive(:find_by_mail).with(@author.mail).and_return(@author)
-
-    mail = Mailer.create_issue_edit(@journal, @author.mail)
-    mail.body.should_not match(/hours/i)
-  end
-
-  it_should_behave_like 'with a Deliverable'
+  it_should_behave_like 'a custom email'
 end
