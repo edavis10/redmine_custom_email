a57415de1cff7bde748f468daa6964f77eefcf98
Eric Davis <edavis@littlestreamsoftware.com>
2009-02-16 10:45:23 -0800

    Added the Deliverable subject to the issue add email. #2136

--- spec/lib/custom_email_mailer_patch_spec.rb
+++ spec/lib/custom_email_mailer_patch_spec.rb
@@ -1,14 +1,22 @@
 require File.dirname(__FILE__) + '/../spec_helper'
 
 describe Mailer, "#issue_add_with_recipient" do
+  before(:each) do
+    @author = mock_model(User, :name => 'Mock', :login => 'mock', :email => 'test@example.com')
+    @project = mock_model(Project, :name => 'test', :identifier => 'test')
+    @issue = Issue.new(:author => @author, :project => @project)
+  end
+
   it 'should use the Redmine method if no recipient is passed'
   
   it 'should set the recipient to only one user' do
-    author = mock_model(User, :name => 'Mock', :login => 'mock', :email => 'test@example.com')
-    project = mock_model(Project, :name => 'test', :identifier => 'test')
-    issue = Issue.new(:author => author, :project => project)
-
-    mail = Mailer.create_issue_add(issue, author.email)
+    mail = Mailer.create_issue_add(@issue, @author.email)
     mail.bcc.size.should eql(1)
+  end
+
+  it 'should display the Deliverable subject if the Budget plugin is present' do
+    @issue.deliverable = mock_model(Deliverable, :subject => "Take over the world")
+    mail = Mailer.create_issue_add(@issue, @author.email)
+    mail.body.should match(/take over the world/i)
   end
 end
