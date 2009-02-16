3c7a35998c34dce4282962cff3181aa7e7864a97
Eric Davis <edavis@littlestreamsoftware.com>
2009-02-16 12:18:10 -0800

    Added a helper to render the spent hours and estimates. #2136

--- app/helpers/custom_email_helper.rb
+++ app/helpers/custom_email_helper.rb
@@ -10,5 +10,13 @@ module CustomEmailHelper
     end
   end
 
+  def issue_spent_hours(user, issue, html=true)
+    return '' unless user && user.allowed_to?(:view_time_entries, issue.project)
 
+    if html
+      return "<li>#{ l(:label_spent_hours) }: #{ issue.spent_hours } of #{ issue.estimated_hours || 0 } #{ l(:field_hours) }</li>"
+    else
+      return "#{ l(:label_spent_hours) }: #{ issue.spent_hours } of #{ issue.estimated_hours || 0 } #{ l(:field_hours) }"
+    end
+  end
 end
