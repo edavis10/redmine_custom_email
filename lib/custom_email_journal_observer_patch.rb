c9a3b9217042a425a0c9236436edfdbaa78c1ff2
Eric Davis <edavis@littlestreamsoftware.com>
2009-08-18 13:24:29 -0700

    [#2918] Include question email addresses in the recipients
    * Include the questionee email address if a new question is assigned
    * Include any email addresses for open questions that will be closed

--- lib/custom_email_journal_observer_patch.rb
+++ lib/custom_email_journal_observer_patch.rb
@@ -5,7 +5,23 @@ module CustomEmailJournalObserverPatch
     if self.send_notification && Setting.notified_events.include?('issue_updated')
       # Recipient and watchers should be emailed
       issue = journal.journalized
-      (issue.recipients + issue.watcher_recipients).uniq.each do |recipient|
+      recipients = (issue.recipients + issue.watcher_recipients)
+
+      # Add in the question recipient
+      if journal.question && journal.question.assigned_to && journal.question.assigned_to.mail
+        recipients << journal.question.assigned_to.mail
+      end
+
+      # Add in any question askers.
+      if issue.pending_question?(journal.user)
+        issue.open_questions.all.each do |question|
+          if question.assigned_to == journal.user && question.author && question.author.mail
+            recipients << question.author.mail
+          end
+        end
+      end
+
+      recipients.uniq.each do |recipient|
         Mailer.deliver_issue_edit(journal, recipient)
       end
     end
