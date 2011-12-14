a2c2830366868cf97c2c7732acbeb66e8bae3b38
Eric Davis <edavis@littlestreamsoftware.com>
2011-12-13 16:27:33 -0800

    [#6717 SP#14245] Allow question reminder emails to be sent

--- lib/custom_email_question_mailer_patch.rb
+++ lib/custom_email_question_mailer_patch.rb
@@ -9,7 +9,15 @@ module CustomEmail
       module InstanceMethods
         # Make sure the email isn't delivered
         def deliver!(mail = @mail)
-          return true
+          type_header = mail.header["x-redmine-type"] || mail.header["x-chiliproject-type"]
+          
+          if type_header.present? && type_header.to_s == "Question"
+            # Reminder email so allow sending
+            super
+          else
+            # Question details email, don't send
+            return true
+          end
         end
       end
       
