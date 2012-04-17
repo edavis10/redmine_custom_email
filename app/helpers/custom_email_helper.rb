859fd5527bc3cbc04d6961571783adbfd5ba9a97
Eric Davis <edavis@littlestreamsoftware.com>
2012-04-17 09:06:42 -0700

    [#6898] Update email to use the Contracts API

--- app/helpers/custom_email_helper.rb
+++ app/helpers/custom_email_helper.rb
@@ -4,18 +4,18 @@ module CustomEmailHelper
     return '' unless Object.const_defined?("Deliverable") && @issue.deliverable
       
     if html
-      return "<li>#{ l(:field_deliverable_subject) }: #{ @issue.deliverable.subject }</li>"
+      return "<li>#{ l(:field_deliverable_title) }: #{ @issue.deliverable.title }</li>"
     else
-      return "#{ l(:field_deliverable_subject) }: #{ @issue.deliverable.subject }"
+      return "#{ l(:field_deliverable_title) }: #{ @issue.deliverable.title }"
     end
   end
 
   def deliverable_labor_budget(user, issue, html=true)
     return '' unless Object.const_defined?("Deliverable") && issue.deliverable && user && user.allowed_to?(:manage_budget, issue.project)
 
-    text = "#{ l(:label_labor_budget_spent) }: " # Label
-    text += "#{issue.deliverable.hours_used} #{ l(:field_hours) } (#{ number_to_currency(issue.deliverable.spent) }) " # Used
-    text += "#{issue.deliverable.total_hours} #{ l(:field_hours) } (#{ number_to_currency(issue.deliverable.labor_budget) }) " # Total 
+    text = "#{ l(:field_labor_spent) }: " # Label
+    text += "#{issue.deliverable.hours_spent_total} #{ l(:field_hours) } (#{ number_to_currency(issue.deliverable.total_spent) }) " # Used
+    text += "#{issue.deliverable.estimated_hour_budget_total} #{ l(:field_hours) } (#{ number_to_currency(issue.deliverable.labor_budget_total) }) " # Total 
 
     if html
       return content_tag(:li, text)
